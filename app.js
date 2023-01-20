e169a4cb77858e0f2a0f74deaa27c891ddb4a6ff
Jeff Goodall <goodalls@live.com>
2023-01-20 12:01:22 -0700

    added shapes switch

--- app.js
+++ app.js
@@ -25,26 +25,49 @@ function control(e) {
 
 
 //single box
-function draw(x, y) {
-    ctx.fillStyle = "blue";
+function draw(x, y, color) {
+    ctx.fillStyle = color;
     ctx.fillRect(x, y, scale, scale);
     ctx.clearRect(x + scale/4, y + scale/4, scale/2, scale/2);
 }
 
 function shape(x,y, type) {
+    //i, o, t, j, l, s, z.
     switch (type) {
-        case square:
-            Array().map((e, i, arr)=>{
-
-            })
-            break;
-    
+        case 'o':
+         Array([x,y], [x+scale, y], [x, y+scale], [x+scale, y+scale]).forEach((e)=>draw(e[0], e[1]))
+        break;
+            
+        case 'i':
+        Array([x,y], [x+scale, y], [x+scale+scale, y], [x+scale+scale+scale, y]).forEach((e)=>draw(e[0], e[1]))
+        break;
+                   
+        case 't':
+        Array([x,y], [x+scale, y], [x+scale*2, y], [x+scale, y+scale]).forEach((e)=>draw(e[0], e[1]))
+        break;
+                       
+        case 'j':
+        Array([x,y+scale*2], [x+scale, y], [x+scale, y+scale], [x+scale, y+scale*2]).forEach((e)=>draw(e[0], e[1]))
+        break;
+                           
+        case 'l':
+        Array([x,y], [x, y+scale], [x, y+scale*2], [x+scale, y+scale*2]).forEach((e)=>draw(e[0], e[1]))
+        break;
+                               
+        case 's':
+        Array([x,y+scale], [x+scale, y+scale], [x+scale, y], [x+scale*2, y]).forEach((e)=>draw(e[0], e[1]))
+        break;
+        
+        case 'z':
+        Array([x,y], [x+scale, y], [x+scale, y+scale], [x+scale*2, y+scale]).forEach((e)=>draw(e[0], e[1]))
+        break;
+ 
         default:
             break;
     }
 }
 
-shape(10, 10, square);
+shape(10, 10, 's');
 
 
 window.addEventListener("keydown", control);
