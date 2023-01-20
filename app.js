69a845c91b876451540513be59cd2fc5797b9a09
Jeff Goodall <goodalls@live.com>
2023-01-20 10:36:50 -0700

    made draw() function

--- app.js
+++ app.js
@@ -1,6 +1,50 @@
 let c = document.getElementById("canvas");
 let ctx = c.getContext("2d");
+let scale = 20;
 
-ctx.beginPath();
-ctx.fillRect(20, 20, 150, 100);
-ctx.stroke();
+function control(e) {
+  switch (e.keyCode) {
+    case 37:
+      console.log("arrowLeft");
+      break;
+    case 40:
+      console.log("arrowDown");
+      break;
+    case 38:
+      console.log("ArrowUp");
+      break;
+    case 39:
+      console.log("arrowRight");
+      break;
+
+    default:
+      console.log("uncaptured keyDown event");
+      break;
+  }
+}
+
+
+//single box
+function draw(x, y) {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(x, y, scale, scale);
+    ctx.clearRect(x + scale/4, y + scale/4, scale/2, scale/2);
+}
+
+function shape(x,y, type) {
+    switch (type) {
+        case square:
+            Array().map((e, i, arr)=>{
+
+            })
+            break;
+    
+        default:
+            break;
+    }
+}
+
+shape(10, 10, square);
+
+
+window.addEventListener("keydown", control);
